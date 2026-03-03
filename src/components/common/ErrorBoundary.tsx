35b469d058df6b66bcf1ec84e5ce5b612b7244da
fayas17 <fayas1771@gmail.com>
2026-03-03 20:39:20 +0530

    tested the lint fix for prettier, errors and warnings

--- src/components/common/ErrorBoundary.tsx
+++ src/components/common/ErrorBoundary.tsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React from 'react'
 
 type Props = {
-    children: React.ReactNode;
+    children: React.ReactNode
 }
 
 type State = {
-    hasError: boolean;
+    hasError: boolean
 }
 
 class ErrorBoundary extends React.Component<Props, State> {
     constructor(props: Props) {
-        super(props);
-        this.state = { hasError: false };
+        super(props)
+        this.state = { hasError: false }
     }
 
     static getDerivedStateFromError() {
-        return { hasError: true };
+        return { hasError: true }
     }
 
     render() {
         if (this.state.hasError) {
             return (
-                  <div className="min-h-screen flex items-center justify-center">
-          <h1 className="text-red-500 text-xl">
-            Something went wrong.
-          </h1>
-        </div>
-      )
-    }
+                <div className="min-h-screen flex items-center justify-center">
+                    <h1 className="text-red-500 text-xl">
+                        Something went wrong.
+                    </h1>
+                </div>
+            )
+        }
 
-    return this.props.children;        
+        return this.props.children
+    }
 }
-}
-export default ErrorBoundary;
+export default ErrorBoundary
