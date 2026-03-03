9b0ac56f70062783305185ab23466c57076afdca
fayas17 <fayas1771@gmail.com>
2026-03-03 20:38:00 +0530

    updated the eslint and lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,46 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
+
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig, // disables conflicting rules
     ],
+
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+        settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': 'error',
     },
   },
 ])
