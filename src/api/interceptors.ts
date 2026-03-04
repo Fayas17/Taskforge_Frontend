67c971dfcd816be514b1e34bc6ddedbe9f2a84fb
fayas17 <fayas1771@gmail.com>
2026-03-04 20:23:27 +0530

    feat: add Axios interceptors for attaching authentication tokens and standardizing error responses.

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,5 +1,13 @@
 import { api } from './axios'
 
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access')
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+
 api.interceptors.response.use(
     (response) => response,
     (error) => {
